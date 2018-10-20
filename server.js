df926de4fdab1ae13365378559821ba9f11f53aa
ssolders <simon.solders@devcode.se>
2018-10-20 11:10:55 +0200

    Working basic setup of apollo

--- server.js
+++ server.js
@@ -1,15 +1,60 @@
 const { ApolloServer, gql } = require('apollo-server');
-// const port = 4500
 
 const books = [
   {
+    id: 1,
     title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
+    authorId: 1,
   },
   {
+    id: 2,
     title: 'Jurassic Park',
-    author: 'Michael Crichton',
+    authorId: 2,
   },
+  {
+    id: 3,
+    title: 'Djungelboken',
+    authorId: 3,
+  },
+  {
+    id: 4,
+    title: 'Oliver Twist',
+    authorId: 4,
+  },
+  {
+    id: 5,
+    title: 'The white fleet',
+    authorId: 5,
+  },
+  {
+    id: 6,
+    title: 'Processen',
+    authorId: 5,
+  }
+];
+
+
+const authors = [
+  {
+    id: 1,
+    name: 'J.K Rowling'
+  },
+  {
+    id: 2,
+    name: 'Michael Crichton'
+  },
+  {
+    id: 3,
+    name: 'Rudyard Kipling'
+  },
+  {
+    id: 4,
+    name: 'J Allen'
+  },
+  {
+    id: 5,
+    name: 'Franz Kafka'
+  }
 ];
 
 // Type definitions define the "shape" of your data and specify
@@ -19,25 +64,55 @@ const typeDefs = gql`
 
   # This "Book" type can be used in other type declarations.
   type Book {
+    id: Int
     title: String
-    author: String
+    author: Author
+  }
+
+  type Author {
+    name: String
+    id: Int
+    books: [Book]
   }
 
   # The "Query" type is the root of all GraphQL queries.
   # (A "Mutation" type will be covered later on.)
   type Query {
-    books: [Book]
+    books: [Book],
+    authors: [Author]
+    author: Author
   }
 `;
 
+
 const resolvers = {
   Query: {
-    books: () => books,
+    books: (_, args) => {
+      return books;
+    },
+    authors: (_, args) => authors,
   },
+  Author: {
+    books(author) {
+      console.log('author->books query');
+      return books.filter(book => book.authorId === author.id);
+    }
+  },
+  Book: {
+    author(book) {
+      console.log('book->author query');
+      return authors.find(author => author.id === book.authorId)
+    }
+  }
 };
 
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
 const server = new ApolloServer({ typeDefs, resolvers });
 
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
